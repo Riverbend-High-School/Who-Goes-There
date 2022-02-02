36a707101be457d727f826f20e69a9aaaed95ab2
Gabriel Hogan <Gabriel.T.Hogan@gmail.com>
2022-02-02 09:47:23 -0500

    Implement sentry logging for the frontend

--- frontend/wgt-frontend/src/main.js
+++ frontend/wgt-frontend/src/main.js
@@ -2,6 +2,9 @@ import * as Vue from 'vue' // in Vue 3
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import * as VueRouter from 'vue-router';
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
+
 import App from './App.vue';
 
 import checkTemplate from './components/checkTemplate.vue';
@@ -29,7 +32,25 @@ const router = VueRouter.createRouter({
     ]
 })
 
+Sentry.init({
+    app,
+    dsn: process.env.VUE_APP_SENTRY_DSN,
+    integrations: [
+      new BrowserTracing({
+        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+        tracingOrigins: ["localhost", "my-site-url.com", /^\//],
+      }),
+    ],
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+
 const app = Vue.createApp(App);
 app.use(router);
 app.use(VueAxios, axios)
 app.mount('#app');
+
+app.mixin(Sentry.createTracingMixins({ trackComponents: true }));
+Sentry.attachErrorHandler(app, { logErrors: true });
